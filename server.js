622611d49bc7c6b1551c79c22551af61c9bc0ae2
stephani <paleloco_lauris@hotmail.com>
2016-07-07 08:52:03 -0500

    lab03 IP-PORT server con ip y puerto tomados del entorno

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 // http 
 var http = require("http");
+//obteniendo  informacion del entorno de ejecucion con respecto al ip y al puerto que 
+//que debemos usar en nuestro server 
+var PORT = process.env.PORT || 3000;
+var IP = process.env.IP || '127.0.0.1';
+if(IP=='127.0.0.1'){
+    console.log(">--- ejecutando en modo local----<")
+}
 // Crear un servidor basico 
 var server = http.createServer(function(req,res){
     // armar la respuesta http
@@ -9,11 +16,11 @@ res.writeHead(200,{
     "Server" : "ITGAM@4.2.4"
  });
  // enviamos la respuesta 
- res.write("hola mundo");
+ res.write("hola stephani");
  //cerrar la coneccion 
  res.end();
 });
 //poner a trabajar al server 
-server.listen(300,'127.0.0.1',function(){
-    console.log("> Server listening @http://localhost:300...");
+server.listen(PORT,IP,function(){
+    console.log(`> Server listening @http://${IP}:${PORT}...`);
 });
